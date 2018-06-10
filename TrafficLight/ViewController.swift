f72f38ef8440337b7aed6589b6eae4a108d464ed
Алексей Ефимов <lex.efimov@gmail.com>
2018-06-10 11:26:01 +0500

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Alexey Efimov on 10.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
