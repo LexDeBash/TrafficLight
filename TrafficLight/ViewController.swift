06262aac91c1e74ceaef0fbe5330b637574eafe1
Alexey Efimov <lex.efimov@gmail.com>
2018-07-18 12:54:57 +0500

    Traffic Light finished

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green, off
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var currentLight = CurrentLight.off
+    
+    let lightIsOn: CGFloat = 1
+    let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        redLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
+        
+        redLight.backgroundColor = UIColor.red.withAlphaComponent(lightIsOff)
+        yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(lightIsOff)
+        greenLight.backgroundColor = UIColor.green.withAlphaComponent(lightIsOff)
     }
 
-
+    @IBAction func startButtonPressed(_ sender: Any) {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLight.backgroundColor? = UIColor.red.withAlphaComponent(lightIsOff)
+            yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(lightIsOn)
+            currentLight = .yellow
+        case .yellow:
+            yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(lightIsOff)
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(lightIsOn)
+            currentLight = .green
+        case .green:
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(lightIsOn)
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(lightIsOff)
+            currentLight = .red
+        case .off:
+            redLight.backgroundColor? = UIColor.red.withAlphaComponent(lightIsOn)
+            currentLight = .red
+        }
+    }
+    
 }
 
